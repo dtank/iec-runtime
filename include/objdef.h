e6ccad4f9815347edb4796f9ce9d87b1d6c94e2c
dtank <tblddt@gmail.com>
2014-11-14 15:36:00 +0800

    Delete generator from main

--- include/objdef.h
+++ include/objdef.h
@@ -1,6 +1,7 @@
 #ifndef __OBJDEF_H__
 #define __OBJDEF_H__
 
+#include <stdint.h>
 /*-----------------------------------------------------------------------------
  * PLC Object File Header
  *---------------------------------------------------------------------------*/
@@ -28,5 +29,17 @@
 #define ARG_ADDR_RESERVE 0x03
 #define ARG_ADDR_FLAG_SIZE 2
 #define ARG_ADDR_FLAG_MASK ~(0xFFFFFFFF << 2)
+
+/*-----------------------------------------------------------------------------
+ * Definition of PLC Object File Header
+ *---------------------------------------------------------------------------*/
+typedef struct {
+	char magic[MAGIC_SIZE]; /* magic number */
+	uint8_t type;           /* type of object file: 32BIT | 64BIT */
+	uint8_t order;          /* byte order: LITTLE-ENDIAN | BIG-ENDIAN */
+	uint8_t version;        /* version of object file (default: 1)*/
+	uint8_t machine;        /* CPU platform */
+} OBJ_HEADER;
+
 
 #endif
--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,6 @@
 #include <unistd.h> /* required */
 #include <sys/mman.h>
 
-#include "binformat.h"
-#include "generator.h"
 #include "loader.h"
 #include "executor.h"
 #include "iocontroller.h"
@@ -40,40 +38,10 @@ void sys_exit() {
 }
 int main(int argc, char* argv[])
 {
-    OBJ_FILE obj_file = {
-        {MAGIC, SYS_TYPE_32, BYTE_ORDER_LIT, 1, MACH_CORTEX_A8}, // file header
-        {4000000, 0, 1, 0, 0}, // io configuration
-        {2, 4000000, { // servo configuration
-            {"axis1", false, 1, AXIS_TYPE_FINITE, OPER_MODE_POS,
-			0.0, 180.0, 100.0, 20.0, 20.0, 10.0},
-            {"axis2", true, 2, AXIS_TYPE_FINITE, OPER_MODE_POS,
-			0.0, 180.0, 100.0, 20.0, 20.0, 10.0},
-        }},
-        1, // plc task count
-        {
-            {{"task1", 80, 100000000u, 20, 1},
-            {0x02, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13},{
-            //{STD_ADD, {0x00000001, 0x00000011, 0x00000001}},
-            //{STD_SUB, {0x00000001, 0x00000011, 0x00000001}},
-            //{STD_MUL, {0x00000001, 0x00000011, 0x00000001}},
-            //{STD_ADD, {0x00000001, 0x00000002, 0x00000002}},
-            {STD_LT, {0x00000001, 0x0000002, 0x00000002}},
-            }},
-            //{{"task2", 90, 500000000u, 20, 1},
-            //{0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13},{
-            //{STD_ADD, {0x00000001, 0x00000011, 0x00000001}},
-            //}},
-        }
-    };
-
 	//Avoids memory swapping for this program
     mlockall(MCL_CURRENT|MCL_FUTURE);
 
-	FILE *fplc = fopen("plc.bin", "wb");
-    generate_obj_file(fplc, &obj_file, &inst_desc);
-	fclose(fplc);
 	sys_init();
-
     sys_start();
     pause();
     sys_exit();
