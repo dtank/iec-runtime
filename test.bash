a12ed0aa6e0f7a2698a9a6f601ef1526fe92d212
dtank <tblddt@gmail.com>
2014-11-17 18:28:51 +0800

    Add onekey script(build & test)
    NOTE:
     - Modify test script
     - Better to check arguments in *main_genobj.cc*

--- test.bash
+++ test.bash
@@ -3,8 +3,12 @@
 EXE=./bin/iec-runtime
 OBJ=plc.bin
 NFSDIR=~/nfs
+GENOBJ=./bin/generate_obj
 
-./bin/generate_obj arithmetic
+# Step1: generate obj file
+${GENOBJ} "$1"
+
+# Step2: move executor & obj file to NFS directory
 cp ${EXE} ${NFSDIR}
 mv ${OBJ} ${NFSDIR}
 
